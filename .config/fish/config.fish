c3099aa1a75c2bb4698cb8b31752897ad9d41422
TmLev <sololev27@gmail.com>
2021-09-02 13:53:22 +0300

    fix: Empty paths before setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,13 @@ alias ls=exa
 alias grep=rg
 
 ## Paths
+# Reset everything before adding anything
+set -x fish_user_paths
 # Rust / Cargo
 set -Ua fish_user_paths $HOME/.cargo/bin
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -Ua fish_user_paths $PYENV_ROOT/bin
 # Yandex Cloud
 set -Ua fish_user_paths /Users/lev/yandex-cloud/bin
 # Haskell language server
@@ -21,14 +26,11 @@ set -Ua fish_user_paths /Users/lev/.local/bin
 starship init fish | source
 
 # `pyenv` must be close to the end also, but after `starship`
-pyenv init - | source
+status is-interactive; and pyenv init --path | source
 
 # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 test -f /Users/lev/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/lev/.ghcup/bin $PATH
-
-# tmux session
-# tmux new-session -A -s main
 
 # zoxide - `z`-like navigation
 zoxide init fish | source
