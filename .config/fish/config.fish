78239f7a884d0ac2f023d531eefd6ea146c0aebd
TmLev <sololev27@gmail.com>
2021-12-24 15:00:50 +0300

    refactor: Better `fish` config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,48 @@
-## Aliases
+# ------------------------------------------------------------------------------
+# Aliases
+
 # neovim
 alias vim=nvim
+
 # Docker
 alias dc=docker-compose
+
 # Rust utils
 alias ls=exa
 alias grep=rg
 
-## Paths
+# vcpkg
+alias vcpkg=$HOME/repos/vcpkg/vcpkg
+
+# ------------------------------------------------------------------------------
+# Paths
+
 # Reset everything before adding anything
 set -x fish_user_paths
+
 # Rust / Cargo
 set -Ua fish_user_paths $HOME/.cargo/bin
+
 # pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -Ua fish_user_paths $PYENV_ROOT/bin
-# Yandex Cloud
-set -Ua fish_user_paths /Users/lev/yandex-cloud/bin
-# Haskell language server
-set -Ua fish_user_paths /Users/lev/.haskell-ls
-# local bin
-set -Ua fish_user_paths /Users/lev/.local/bin
 
-# `starship` initialization must be close to the end
+# Yandex Cloud
+set -Ua fish_user_paths $HOME/yandex-cloud/bin
+
+# Haskell language server
+set -Ua fish_user_paths $HOME/.haskell-ls
+
+# local bin
+set -Ua fish_user_paths $HOME/.local/bin
+
+# ------------------------------------------------------------------------------
+# Initialization
+
+# zoxide - `z`-like navigation
+zoxide init fish | source
+
+# `starship`, must be close to the end
 starship init fish | source
 
 # `pyenv` must be close to the end also, but after `starship`
@@ -30,7 +50,5 @@ status is-interactive; and pyenv init --path | source
 
 # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-test -f /Users/lev/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/lev/.ghcup/bin $PATH
+test -f $HOME/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
 
-# zoxide - `z`-like navigation
-zoxide init fish | source
